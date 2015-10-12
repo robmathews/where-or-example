a5cb764d5cbd802f1033cb595ab5d44cd1fa6b15
Alex Coles <alex@alexbcoles.com>
2015-10-12 10:58:21 +0200

    Add model associations
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,6 @@
 class Offer < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :available_product, -> { where(available: true) },
+             class_name:  'Product',
+             foreign_key: :product_id
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   validates :title, presence: true
+  has_many :offers
+  has_many :legitimate_offers, -> { where(legitimate: true) }, class_name: 'Offer'
 end
